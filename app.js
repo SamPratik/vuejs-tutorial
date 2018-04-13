b1906137f7b8f3fc92f8da1395cafa467f939dfa
SamPratik <pratik.anwar@gmail.com>
2018-04-13 22:55:36 +0600

    keyboard events

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 new Vue({
   el: '#app',
   data: {
-    age: 23,
-    x: 0,
-    y: 0
+
   },
   methods: {
-    greet: function(time) {
-      return 'Good ' + time + ', ' + this.name;
+    logName: function() {
+      console.log('You entered a name!');
     },
-    addYear: function(inc) {
-      this.age += inc;
-    },
-    subtractYear: function(dec) {
-      this.age -= dec;
-    },
-    updateXY: function(event) {
-      console.log(event);
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click: function() {
-      alert('This is a click');
+    logAge: function() {
+      console.log('You entered an age!');
     }
   }
 });
