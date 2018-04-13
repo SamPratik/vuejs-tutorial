62301357f333015220e0a2b7ea795091f08b792d
SamPratik <pratik.anwar@gmail.com>
2018-04-13 12:29:47 +0600

    Data and Methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Shaun'
+    name: 'Shaun',
+    job: 'Ninja'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ', ' + this.name;
+    }
   }
 });
