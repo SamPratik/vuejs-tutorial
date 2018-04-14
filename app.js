fa9ef0bee14c72e58b82d133021d14fcece10e22
SamPratik <pratik.anwar@gmail.com>
2018-04-14 22:22:51 +0600

    Refs

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-// This is component named 'greeting' which is reused in both vue instances...
-Vue.component('greeting', {
-  template: '<p>Hey there, I am <strong>{{name}}</strong> <button v-on:click="changeName">Change Name</button></p>',
-  data: function() {
-    return {
-      name: 'Pratik'
-    }
-  },
-  methods: {
-    changeName: function() {
-      this.name = 'Samiul Alim Pratik';
-    }
-  }
-});
-
-var one = new Vue({
-  el: '#vue-app-one',
+new Vue({
+  el: '#vue-app',
   data: {
-
+    output: 'Your favorite food'
   },
   methods: {
-
+    readRefs: function() {
+      // Showing all the references...
+      console.log(this.$refs);
+      // changing the value of output with input value...
+      this.output = this.$refs.input.value;
+    }
   }
-});
-
-var two = new Vue({
-  el: '#vue-app-two',
-  data: {
-
-  },
-  methods: {
-
-  }
-});
+})
