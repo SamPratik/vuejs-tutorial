efe2c2f855cfcb35d62fe0fc6b7582d76d62a22f
SamPratik <pratik.anwar@gmail.com>
2018-04-14 09:43:00 +0600

    Computed Properties

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 new Vue({
   el: '#app',
   data: {
-
+    a: 0,
+    b: 0,
+    age: 20
   },
-  methods: {
-    logName: function() {
-      console.log('You entered a name!');
+  // if any of the variables(a/b) is updated both of the functions are
+  // getting called which is not required. we only need to call that method
+  // whose variable has been updated. we can use computed property instead
+  // of methods...
+  /*methods: {
+    addToA: function() {
+      console.log('addToA');
+      return this.age + this.a;
     },
-    logAge: function() {
-      console.log('You entered an age!');
+    addToB: function() {
+      console.log('addToB');
+      return this.age + this.b;
+    }
+  }*/
+  computed: {
+    addToA: function() {
+      console.log('addToA');
+      return this.age + this.a;
+    },
+    addToB: function() {
+      console.log('addToB');
+      return this.age + this.b;
     }
   }
 });
