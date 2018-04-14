9a406e6692f40894a3f2dcbadd1587d7f2072e6d
SamPratik <pratik.anwar@gmail.com>
2018-04-14 10:10:18 +0600

    Dynamic CSS Classes

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 new Vue({
   el: '#app',
   data: {
-    a: 0,
-    b: 0,
-    age: 20
+    available: false,
+    nearby: false
   },
-  // if any of the variables(a/b) is updated both of the functions are
-  // getting called which is not required. we only need to call that method
-  // whose variable has been updated. we can use computed property instead
-  // of methods...
-  /*methods: {
-    addToA: function() {
-      console.log('addToA');
-      return this.age + this.a;
-    },
-    addToB: function() {
-      console.log('addToB');
-      return this.age + this.b;
-    }
-  }*/
+
   computed: {
-    addToA: function() {
-      console.log('addToA');
-      return this.age + this.a;
-    },
-    addToB: function() {
-      console.log('addToB');
-      return this.age + this.b;
+    compClasses: function() {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
     }
   }
 });
