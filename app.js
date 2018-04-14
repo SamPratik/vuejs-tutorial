7d68e99b25f318f258f9c4ca90539544ffea50e6
SamPratik <pratik.anwar@gmail.com>
2018-04-14 17:23:51 +0600

    v-for

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    error: false,
-    success: false
-  },
-
-  computed: {
-
+    characters: ['Pratik', 'Affan', 'Shihab', 'Mahir'],
+    ninjas: [
+      {name: 'Pratik', age: 23},
+      {name: 'Affan', age: 24},
+      {name: 'Shihab', age: 27},
+      {name: 'Mahir', age: 28}
+    ]
   }
 });
